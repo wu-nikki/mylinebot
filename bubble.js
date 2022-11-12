4684af858d67e8f54a1ae7f83596f789d37f53a7
wu-nikki <nikki851205@gmail.com>
2022-11-13 01:18:42 +0800

    完成輪播

---
+++ bubble.js
@@ -0,0 +1,116 @@
+export default {
+  type: 'bubble',
+  header: {
+    type: 'box',
+    layout: 'vertical',
+    contents: []
+  },
+  hero: {
+    type: 'image',
+    url: 'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+    size: 'full',
+    aspectRatio: '20:13',
+    aspectMode: 'cover',
+    action: {
+      type: 'uri',
+      uri: 'http://linecorp.com/'
+    }
+  },
+  body: {
+    type: 'box',
+    layout: 'vertical',
+    contents: [
+      {
+        type: 'text',
+        text: 'Brown Cafe',
+        weight: 'bold',
+        size: 'xl'
+      },
+      {
+        type: 'box',
+        layout: 'vertical',
+        margin: 'lg',
+        spacing: 'sm',
+        contents: [
+          {
+            type: 'box',
+            layout: 'baseline',
+            spacing: 'sm',
+            contents: [
+              {
+                type: 'text',
+                color: '#aaaaaa',
+                size: 'sm',
+                flex: 1,
+                text: '我在'
+              },
+              {
+                type: 'text',
+                text: 'Miraina Tower, 4-1-6 Shinjuku, Tokyo',
+                wrap: true,
+                color: '#666666',
+                size: 'sm',
+                flex: 5
+              }
+            ]
+          },
+          {
+            type: 'box',
+            layout: 'baseline',
+            spacing: 'sm',
+            contents: [
+              {
+                type: 'text',
+                text: '地址',
+                color: '#aaaaaa',
+                size: 'sm',
+                flex: 1
+              },
+              {
+                type: 'text',
+                text: '10:00 - 23:00',
+                wrap: true,
+                color: '#666666',
+                size: 'sm',
+                flex: 5
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  footer: {
+    type: 'box',
+    layout: 'vertical',
+    spacing: 'sm',
+    contents: [
+      {
+        type: 'button',
+        style: 'link',
+        height: 'sm',
+        action: {
+          type: 'postback',
+          label: '複製我的資訊',
+          data: 'action=&itemid=111',
+          // displayText: '複製詳細資訊',
+          inputOption: 'openKeyboard',
+          fillInText: ''
+        }
+      },
+      {
+        type: 'button',
+        style: 'link',
+        height: 'sm',
+        action: {
+          type: 'uri',
+          label: '我的網站',
+          uri: ''
+        }
+      }
+    ],
+    flex: 0
+  }
+}
+
+// https://asms.coa.gov.tw/Amlapp/App/AnnounceList.aspx?Id=282171&AcceptNum=1111103C10&PageType=Adopt
--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import 'dotenv/config'
 import linebot from 'linebot'
-// import axios from 'axios'
+import axios from 'axios'
 // import fs from 'fs'
 
-import dd from './json/test.js'
+// import dd from './json/test.js'
+
+import bubble from './bubble.js'
+
+// import animalList from './animalList.js'
 
 const bot = linebot({
   channelId: process.env.CHANNEL_ID,
   channelSecret: process.env.CHANNEL_SECRET,
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 })
-
+// 1.建立一個整理過的資料
+const bubbles = []
 const a = async () => {
-  // const { data } = await axios.get('https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&$top=100&$skip=0')
+  const { data } = await axios.get('https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&$top=100&$skip=0')
+  // const data = dd
   // fs.writeFileSync('test.json', JSON.stringify(data))
 
   // 判斷黃金獵犬
@@ -34,7 +40,9 @@ const a = async () => {
   //   return out
   // })
   // console.log(newList)
-  const msg = dd.map((animal) => {
+
+  // 全國動物收容管理系統 :https://asms.coa.gov.tw/Amlapp/App/Default.aspx
+  const msg = data.map((animal) => {
     const out = {}
     out.img = animal.album_file
     out.size = animal.animal_bodytype === 'SMALL' ? '小型' : (animal.animal_bodytype === 'MEDIUM' ? '中型' : '大型')
@@ -42,15 +50,111 @@ const a = async () => {
     out.variety = (animal.animal_Variety === '混種貓' || (animal.animal_Variety === '混種狗')) ? '米克斯' : animal.animal_Variety
     out.gender = animal.animal_sex === 'M' ? '公' : (animal.animal_sex === 'F' ? '母' : '未輸入')
     out.kind = animal.animal_kind === '狗' ? '犬' : '貓'
+    out.id = animal.animal_subid
     out.place = animal.animal_place
     out.add = animal.shelter_address
     out.tel = animal.shelter_tel
+    out.webId = animal.animal_id
     return out
   })
-  console.log(msg)
+  // fs.writeFileSync('animalList.json', JSON.stringify(msg))
+  // 4.把3移進來 才會一起等
+  // 3.陣列裝bubble 並把2放進來
+  bubbles.length = 0
+  for (let i = 1; i <= 12; i++) {
+    const animal = msg[i]
+    // console.log(animal.size + animal.color + animal.variety + animal.gender + animal.kind)
+    // console.log(animal.place)
+    // console.log(animal.add)
+    const out = JSON.parse(JSON.stringify(bubble))
+    out.hero.url = animal.img || 'https://upload.wikimedia.org/wikipedia/commons/8/83/Solid_white_bordered.svg'
+
+    out.body.contents[0].text = (animal.size + animal.color + animal.variety + animal.gender + animal.kind)
+
+    out.body.contents[1].contents[0].contents[1].text = animal.place
+    out.body.contents[1].contents[1].contents[1].text = animal.add
+
+    const copyText = `---
+\n我的小名:${animal.size + animal.color + animal.variety + animal.gender + animal.kind}\n收容編號:${animal.id}
+\n收容所名稱:${animal.place}  \n收容所電話:${animal.tel} \n收容所地址:${animal.add} \n---`
+
+    out.footer.contents[0].action.fillInText = copyText
+
+    const web = `https://asms.coa.gov.tw/Amlapp/App/AnnounceList.aspx?Id=${animal.webId}&AcceptNum=${animal.id}&PageType=Adopt`
+    out.footer.contents[1].action.uri = web
+    bubbles.push(out)
+  }
+  // fs.writeFileSync('tt2.json', JSON.stringify(bubbles))
 }
+
 a()
 
-bot.listen('/', process.env.PORT || 3000, () => {
+// https://developers.line.biz/en/reference/messaging-api/#postback-action
+// fillInText能複製內容的按鈕
+
+// 3.陣列裝bubble 並把2放進來
+// const bubbles = []
+// for (let i = 1; i <= 4; i++) {
+//   const animal = data[i]
+//   console.log(animal.size + animal.color + animal.variety + animal.gender + animal.kind)
+//   console.log(animal.place)
+//   console.log(animal.add)
+//   const out = JSON.parse(JSON.stringify(bubble))
+//   out.hero.url = animal.img
+
+//   out.body.contents[0].text = (animal.size + animal.color + animal.variety + animal.gender + animal.kind)
+
+//   out.body.contents[1].contents[0].contents[1].text = animal.place
+//   out.body.contents[1].contents[1].contents[1].text = animal.add
+
+//   const copyText = `---
+// \n我的小名:${animal.size + animal.color + animal.variety + animal.gender + animal.kind}\n收容編號:${animal.id}
+// \n收容所名稱:${animal.place}  \n收容所電話:${animal.tel} \n收容所地址:${animal.add} \n---`
+
+//   out.footer.contents[0].action.fillInText = copyText
+
+//   const web = `https://asms.coa.gov.tw/Amlapp/App/AnnounceList.aspx?Id=${animal.webId}&AcceptNum=${animal.id}&PageType=Adopt`
+//   out.footer.contents[1].action.uri = web
+
+//   bubbles.push(out)
+// }
+
+// 2.練習對整理過的資料作處理
+// const animal = animalList[0]
+// console.log(animal.size + animal.color + animal.variety + animal.gender + animal.kind)
+// console.log(animal.place)
+// console.log(animal.add)
+
+// bubble.contents.hero.url = animal.img
+
+// bubble.contents.body.contents[0].text = (animal.size + animal.color + animal.variety + animal.gender + animal.kind)
+
+// bubble.contents.body.contents[1].contents[0].contents[1].text = animal.place
+// bubble.contents.body.contents[1].contents[1].contents[1].text = animal.add
+
+// const copyText = `---
+// \n我的小名:${animal.size + animal.color + animal.variety + animal.gender + animal.kind}\n收容編號:${animal.id}
+// \n收容所名稱:${animal.place}  \n收容所電話:${animal.tel} \n收容所地址:${animal.add} \n---`
+
+// bubble.contents.footer.contents[0].action.fillInText = copyText
+
+// const web = `https://asms.coa.gov.tw/Amlapp/App/AnnounceList.aspx?Id=${animal.webId}&AcceptNum=${animal.id}&PageType=Adopt`
+// bubble.contents.footer.contents[1].action.uri = web
+bot.on('message', e => {
+  if (e.message.type !== 'text') return
+  e.reply([
+    { type: 'text', text: e.message.text },
+    {
+      type: 'flex',
+      altText: 'this is a flex message',
+      contents: {
+        type: 'carousel',
+        contents: bubbles
+      }
+    }
+  ])
+})
+
+bot.listen('/', process.env.PORT || 3001, () => {
   console.log('機器人啟動')
 })
