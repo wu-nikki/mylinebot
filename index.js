bf6ab19f55a880aa7602dba62882928e69bd50a6
wu-nikki <nikki851205@gmail.com>
2022-11-22 18:54:18 +0800

    把推撥關掉

--- index.js
+++ index.js
@@ -45,107 +45,107 @@ const init = async () => {
 }
 init()
 
-// 抓當下資料並廣播
-const broadcast = async () => {
-  // 抓當下資料並更新
-  await init()
-  // const data = dd
-  // fs.writeFileSync('test.json', JSON.stringify(data))
+// // 抓當下資料並廣播
+// const broadcast = async () => {
+//   // 抓當下資料並更新
+//   await init()
+//   // const data = dd
+//   // fs.writeFileSync('test.json', JSON.stringify(data))
 
-  // 判斷黃金獵犬
-  // const animalVariety = dd.filter((it) => {
-  //   return it.animal_Variety === '黃金獵犬'
-  // })
+//   // 判斷黃金獵犬
+//   // const animalVariety = dd.filter((it) => {
+//   //   return it.animal_Variety === '黃金獵犬'
+//   // })
 
-  // 判斷顏色 .map把資料做加工辨識
-  // const newList = dd.map((animal) => {
-  //   return animal.animal_colour
-  // })
-  // console.log(newList)
-  // const total
+//   // 判斷顏色 .map把資料做加工辨識
+//   // const newList = dd.map((animal) => {
+//   //   return animal.animal_colour
+//   // })
+//   // console.log(newList)
+//   // const total
 
-  // const newList = dd.map((animal) => {
-  //   const out = {}
-  //   out.c = animal.animal_colour
-  //   out.kind = animal.animal_kind === '貓' ? '貓咪' : '狗狗'
-  //   return out
-  // })
-  // console.log(newList)
+//   // const newList = dd.map((animal) => {
+//   //   const out = {}
+//   //   out.c = animal.animal_colour
+//   //   out.kind = animal.animal_kind === '貓' ? '貓咪' : '狗狗'
+//   //   return out
+//   // })
+//   // console.log(newList)
 
-  // 全國動物收容管理系統 :https://asms.coa.gov.tw/Amlapp/App/Default.aspx
-  // const msg = data.map(animal => {
-  //   const out = {}
-  //   out.img = animal.album_file
-  //   out.size = animal.animal_bodytype === 'SMALL' ? '小型' : (animal.animal_bodytype === 'MEDIUM' ? '中型' : '大型')
-  //   out.color = animal.animal_colour
-  //   out.variety = (animal.animal_Variety === '混種貓' || (animal.animal_Variety === '混種狗')) ? '米克斯' : animal.animal_Variety
-  //   out.gender = animal.animal_sex === 'M' ? '公' : (animal.animal_sex === 'F' ? '母' : '未輸入')
-  //   out.kind = animal.animal_kind === '狗' ? '犬' : (animal.animal_kind === '貓' ? '貓' : '其他')
-  //   // 此id為收容編號
-  //   out.id = animal.animal_subid
-  //   out.place = animal.animal_place
-  //   out.add = animal.shelter_address
-  //   out.tel = animal.shelter_tel
-  //   out.webId = animal.animal_id
-  //   return out
-  // })
+//   // 全國動物收容管理系統 :https://asms.coa.gov.tw/Amlapp/App/Default.aspx
+//   // const msg = data.map(animal => {
+//   //   const out = {}
+//   //   out.img = animal.album_file
+//   //   out.size = animal.animal_bodytype === 'SMALL' ? '小型' : (animal.animal_bodytype === 'MEDIUM' ? '中型' : '大型')
+//   //   out.color = animal.animal_colour
+//   //   out.variety = (animal.animal_Variety === '混種貓' || (animal.animal_Variety === '混種狗')) ? '米克斯' : animal.animal_Variety
+//   //   out.gender = animal.animal_sex === 'M' ? '公' : (animal.animal_sex === 'F' ? '母' : '未輸入')
+//   //   out.kind = animal.animal_kind === '狗' ? '犬' : (animal.animal_kind === '貓' ? '貓' : '其他')
+//   //   // 此id為收容編號
+//   //   out.id = animal.animal_subid
+//   //   out.place = animal.animal_place
+//   //   out.add = animal.shelter_address
+//   //   out.tel = animal.shelter_tel
+//   //   out.webId = animal.animal_id
+//   //   return out
+//   // })
 
-  // fs.writeFileSync('animalList.json', JSON.stringify(msg))
-  // 4.把3移進來 才會一起等
-  // 3.陣列裝bubble 並把2放進來
-  bubbles.length = 0
-  // 隨機號碼0~999 最大999
-  const r = Math.floor(Math.random() * 100)
-  for (let i = r; i < (r + 12); i++) {
-    let it = i
-    if (i > 100) {
-      it = i - 100
-    }
-    // console.log(it)
-    const animal = todayData[it]
-    // console.log(animal.size + animal.color + animal.variety + animal.gender + animal.kind)
-    // console.log(animal.place)
-    // console.log(animal.add)
-    const out = JSON.parse(JSON.stringify(bubble))
-    out.hero.url = animal.img || 'https://upload.wikimedia.org/wikipedia/commons/8/83/Solid_white_bordered.svg'
+//   // fs.writeFileSync('animalList.json', JSON.stringify(msg))
+//   // 4.把3移進來 才會一起等
+//   // 3.陣列裝bubble 並把2放進來
+//   bubbles.length = 0
+//   // 隨機號碼0~999 最大999
+//   const r = Math.floor(Math.random() * 100)
+//   for (let i = r; i < (r + 12); i++) {
+//     let it = i
+//     if (i > 100) {
+//       it = i - 100
+//     }
+//     // console.log(it)
+//     const animal = todayData[it]
+//     // console.log(animal.size + animal.color + animal.variety + animal.gender + animal.kind)
+//     // console.log(animal.place)
+//     // console.log(animal.add)
+//     const out = JSON.parse(JSON.stringify(bubble))
+//     out.hero.url = animal.img || 'https://upload.wikimedia.org/wikipedia/commons/8/83/Solid_white_bordered.svg'
 
-    out.body.contents[0].text = (animal.size + animal.color + animal.variety + animal.gender + animal.kind)
+//     out.body.contents[0].text = (animal.size + animal.color + animal.variety + animal.gender + animal.kind)
 
-    out.body.contents[1].contents[0].contents[1].text = animal.place
-    out.body.contents[1].contents[1].contents[1].text = animal.add
+//     out.body.contents[1].contents[0].contents[1].text = animal.place
+//     out.body.contents[1].contents[1].contents[1].text = animal.add
 
-    const copyText = `---
-\n我的小名:${animal.size + animal.color + animal.variety + animal.gender + animal.kind}\n收容編號:${animal.id}
-\n收容所名稱:${animal.place}  \n收容所電話:${animal.tel} \n收容所地址:${animal.add} \n---`
+//     const copyText = `---
+// \n我的小名:${animal.size + animal.color + animal.variety + animal.gender + animal.kind}\n收容編號:${animal.id}
+// \n收容所名稱:${animal.place}  \n收容所電話:${animal.tel} \n收容所地址:${animal.add} \n---`
 
-    out.footer.contents[0].action.fillInText = copyText
+//     out.footer.contents[0].action.fillInText = copyText
 
-    const web = `https://asms.coa.gov.tw/Amlapp/App/AnnounceList.aspx?Id=${animal.webId}&AcceptNum=${animal.id}&PageType=Adopt`
-    out.footer.contents[1].action.uri = web
-    out.hero.action.uri = web
-    bubbles.push(out)
-  }
-  // fs.writeFileSync('tt2.json', JSON.stringify(bubbles))
+//     const web = `https://asms.coa.gov.tw/Amlapp/App/AnnounceList.aspx?Id=${animal.webId}&AcceptNum=${animal.id}&PageType=Adopt`
+//     out.footer.contents[1].action.uri = web
+//     out.hero.action.uri = web
+//     bubbles.push(out)
+//   }
+//   // fs.writeFileSync('tt2.json', JSON.stringify(bubbles))
 
-  bot.broadcast([
-    // { type: 'text', text: '目前是ngrok測試' },
-    { type: 'text', text: '早安~我們來啦~~~' },
-    {
-      type: 'flex',
-      altText: '毛孩推播~',
-      contents: {
-        type: 'carousel',
-        contents: bubbles
-      }
-    }
+//   bot.broadcast([
+//     // { type: 'text', text: '目前是ngrok測試' },
+//     { type: 'text', text: '早安~我們來啦~~~' },
+//     {
+//       type: 'flex',
+//       altText: '毛孩推播~',
+//       contents: {
+//         type: 'carousel',
+//         contents: bubbles
+//       }
+//     }
 
-  ]
-  )
-}
-// TMD"每天"
-scheduleJob(
-  ' 00 08 1 * *', broadcast
-)
+//   ]
+//   )
+// }
+
+// scheduleJob(
+//   ' 00 08 1 * *', broadcast
+// )
 
 // -------------毛孩顏色-------------
 
